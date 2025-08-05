4f51d2ba5797d54f1a1d6e7e4a1f80429ed78a9b
leeejjju <leeejjju@gmail.com>
2025-08-05 16:13:47 +0900

    views.py urls.py modified

--- ecstest/views.py
+++ ecstest/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
 
-def health_check(request):
-        return HttpResponse("ok", status=200)
