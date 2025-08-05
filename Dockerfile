2b3bbd4224f146d3fcd33280eb0e8eaf93a9541e
leeejjju <leeejjju@gmail.com>
2025-08-05 16:34:34 +0900

    health check added

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:3.12-slim
 
+RUN apt-get update && apt-get install -y gcc netcat && apt-get clean
 WORKDIR /usr/src/app
 COPY . .
-
 RUN pip install -r requirements.txt 
 
 EXPOSE 80
--- ecstest/views.py
+++ ecstest/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 def index(request):
     return render(request, 'index.html')
